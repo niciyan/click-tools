15d4403244e20669bc9b3e10e18e74cab1f79a8c
niciyan <nishiyamasu@gmail.com>
2018-03-23 02:19:59 +0900

    refactor.

--- aws.py
+++ aws.py
@@ -1,44 +1,38 @@
 import click, boto3
 
+class MyException(Exception):
+    pass
+
 def main():
-    ec2 = boto3.client('ec2')
+    try:
+        ec2 = boto3.client('ec2')
 
-    print('Fetching your vpcs. Wait a moment...')
-    vpcs = ec2.describe_vpcs()['Vpcs']
+        print('Fetching your vpcs. Wait a moment...')
+        vpcs = ec2.describe_vpcs()['Vpcs']
+        your_vpc = handle_vpcs(vpcs)
+        response = ec2.describe_subnets(
+                Filters=[ { 'Name':'vpc-id', 'Values': [ your_vpc] }, ]
+                )
+        subnets = response['Subnets']
+        handle_subnets(subnets)
+        print('\n----done.')
+    except Exception as e:
+        print(e)
 
-    if len(vpcs) > 0:
-        pass
-    else:
-        print("No vpc available.")
-        return
-
+def handle_vpcs(vpcs):
+    if len(vpcs) == 0:
+        raise MyException("No vpc available.")
     for vpc in vpcs:
         print('  * {}'.format(vpc['VpcId']))
-
     your_vpc = click.prompt('Input your vpc', type=click.Choice([ vpc['VpcId'] for vpc in vpcs ]))
-    # return
+    return your_vpc
 
-    response = ec2.describe_subnets(
-            Filters=[
-                {
-                    'Name':'vpc-id',
-                    'Values': [
-                        your_vpc,
-                        ]
-                    },
-                ]
-            )
+def handle_subnets(subnets):
+    if len(subnets) == 0:
+        raise MyException("No subnet available.")
+    for subnet in subnets:
+        print('  * {}'.format(subnet['SubnetId']))
 
-    subnets = response['Subnets']
-
-    if len(subnets) > 0:
-        for subnet in subnets:
-            print('  * {}'.format(subnet['SubnetId']))
-    else:
-        print('No subnet available.')
-        return
-
-    print('\n----done.')
 
 if __name__ == '__main__':
     main()
